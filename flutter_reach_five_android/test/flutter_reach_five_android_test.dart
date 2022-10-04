b31b19b1a24a0fe2a77ab93fabdaf5340607df75
Valentin Marquis <valentinm@bam.tech>
2022-10-04 11:50:46 +0200

    chore(comment): remove gen comments

--- flutter_reach_five_android/test/flutter_reach_five_android_test.dart
+++ flutter_reach_five_android/test/flutter_reach_five_android_test.dart
@@ -1,10 +1,3 @@
-// Copyright (c) 2022, Very Good Ventures
-// https://verygood.ventures
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
-
 import 'package:flutter/services.dart';
 import 'package:flutter_reach_five_android/flutter_reach_five_android.dart';
 import 'package:flutter_reach_five_platform_interface/flutter_reach_five_platform_interface.dart';
