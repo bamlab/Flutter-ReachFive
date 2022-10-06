c7907a1c7eedcfb7d8e27cc4d1b0519f2e0a76f1
Valentin Marquis <valentinm@bam.tech>
2022-10-06 15:00:23 +0200

    chore(android): implement loginWithProvider method

--- flutter_reach_five_android/android/src/main/kotlin/tech/bam/flutter_reach_five/android/FlutterReachFivePlugin.kt
+++ flutter_reach_five_android/android/src/main/kotlin/tech/bam/flutter_reach_five/android/FlutterReachFivePlugin.kt
@@ -1,15 +1,30 @@
 package tech.bam.flutter_reach_five.android
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import co.reachfive.identity.sdk.core.ReachFive
+import co.reachfive.identity.sdk.core.models.AuthToken
+import co.reachfive.identity.sdk.core.models.ReachFiveError
 import co.reachfive.identity.sdk.core.models.SdkConfig
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import co.reachfive.identity.sdk.core.models.requests.UpdatePasswordRequest.*
+import co.reachfive.identity.sdk.core.utils.Success
+import co.reachfive.identity.sdk.core.utils.Failure
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
+import io.flutter.plugin.common.PluginRegistry
 
-class FlutterReachFivePlugin : FlutterPlugin, ReachFiveApi.ReachFiveHostApi
+class FlutterReachFivePlugin : FlutterPlugin, PluginRegistry.ActivityResultListener, ActivityAware, ReachFiveApi.ReachFiveHostApi
 {
     private var context: Context? = null
+    private var activity: Activity? = null
+    private var activityBinding: ActivityPluginBinding? = null
+
     private var reachFiveInstances = mutableMapOf<String, ReachFive>()
+    private var loginWithProviderReachFiveInstance: ReachFive? = null
+    private var onLoginWithProviderSuccess: Success<AuthToken>? = null
+    private var onLoginWithProviderFailure: Failure<ReachFiveError>? = null
 
     override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
         ReachFiveApi.ReachFiveHostApi.setup(flutterPluginBinding.binaryMessenger, this)
@@ -18,6 +33,67 @@ class FlutterReachFivePlugin : FlutterPlugin, ReachFiveApi.ReachFiveHostApi
 
     override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
         ReachFiveApi.ReachFiveHostApi.setup(binding.binaryMessenger, null)
+        context = null
+    }
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        activity = binding.activity
+        activityBinding = binding
+        activityBinding?.addActivityResultListener(this)
+    }
+
+    override fun onDetachedFromActivity() {
+        activity = null
+        activityBinding?.removeActivityResultListener(this)
+        activityBinding = null
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+        this.onDetachedFromActivity()
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        this.onAttachedToActivity(binding)
+    }
+
+    private fun resetLoginWithProviderVars() {
+        loginWithProviderReachFiveInstance = null
+        onLoginWithProviderSuccess = null
+        onLoginWithProviderFailure = null
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?): Boolean {
+        val loginWithProviderReachFiveInstance = this.loginWithProviderReachFiveInstance
+        val activity = this.activity
+        val onLoginWithProviderSuccess = this.onLoginWithProviderSuccess
+        val onLoginWithProviderFailure = this.onLoginWithProviderFailure
+
+        if (loginWithProviderReachFiveInstance == null || onLoginWithProviderSuccess == null || onLoginWithProviderFailure ==null) {
+            return true
+        }
+
+        if (activity == null) {
+            onLoginWithProviderFailure(ReachFiveError("No android activity attached to your application"))
+            resetLoginWithProviderVars()
+            return true
+        }
+
+        loginWithProviderReachFiveInstance.onLoginActivityResult(
+            requestCode = requestCode,
+            resultCode = resultCode,
+            intent = data,
+            success = { authToken ->
+                onLoginWithProviderSuccess(authToken)
+                resetLoginWithProviderVars()
+            },
+            failure = { error ->
+                onLoginWithProviderFailure(error)
+                resetLoginWithProviderVars()
+            },
+            activity = activity
+        )
+
+        return true
     }
 
     private fun getReachFiveInstanceKey(reachFiveKey: ReachFiveApi.ReachFiveKeyInterface): String {
@@ -128,6 +204,44 @@ class FlutterReachFivePlugin : FlutterPlugin, ReachFiveApi.ReachFiveHostApi
                 result?.success(Converters.authTokenToInterface(authToken))
             },
             failure = { error -> result?.error(error)}
+        )
+    }
+
+    override fun loginWithProvider(
+        request: ReachFiveApi.LoginWithProviderRequestInterface,
+        result: ReachFiveApi.Result<ReachFiveApi.AuthTokenInterface>?
+    ) {
+        val reachFive: ReachFive
+        try {
+            reachFive = getReachFiveInstance(reachFiveKey = request.reachFiveKey)
+        } catch (error: Error) {
+            result?.error(error)
+            return
+        }
+
+        val provider = reachFive.getProvider(request.provider)
+        if(provider == null) {
+            result?.error(Error("This provider is not available in this reachFive instance"))
+            return
+        }
+
+        val activity = this.activity
+        if(activity == null) {
+            result?.error(Error("No android activity attached to your application"))
+            return
+        }
+
+        this.loginWithProviderReachFiveInstance = reachFive
+        this.onLoginWithProviderSuccess = {
+                authToken ->
+            result?.success(Converters.authTokenToInterface(authToken))
+        }
+        this.onLoginWithProviderFailure = {error -> result?.error(error)}
+
+        provider.login(
+            origin = request.origin,
+            scope = request.scope?.toList() ?: listOf(),
+            activity = activity
         )
     }
 
