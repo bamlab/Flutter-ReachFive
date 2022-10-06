82e7e292b8aa2e97d1470bc790dc3aaf529d8edc
Valentin Marquis <valentinm@bam.tech>
2022-10-06 14:48:17 +0200

    chore(android): use androidScheme instead of scheme

--- flutter_reach_five_android/android/src/main/kotlin/tech/bam/flutter_reach_five/android/FlutterReachFivePlugin.kt
+++ flutter_reach_five_android/android/src/main/kotlin/tech/bam/flutter_reach_five/android/FlutterReachFivePlugin.kt
@@ -21,7 +21,7 @@ class FlutterReachFivePlugin : FlutterPlugin, ReachFiveApi.ReachFiveHostApi
     }
 
     private fun getReachFiveInstanceKey(reachFiveKey: ReachFiveApi.ReachFiveKeyInterface): String {
-        var key = reachFiveKey.sdkConfig.domain + reachFiveKey.sdkConfig.clientId + reachFiveKey.sdkConfig.scheme
+        var key = reachFiveKey.sdkConfig.domain + reachFiveKey.sdkConfig.clientId + reachFiveKey.sdkConfig.androidScheme
         reachFiveKey.providerCreators.forEach { providerCreatorInterface ->
             key += providerCreatorInterface.type.name
         }
@@ -40,7 +40,7 @@ class FlutterReachFivePlugin : FlutterPlugin, ReachFiveApi.ReachFiveHostApi
             sdkConfig = SdkConfig(
                 domain = reachFiveKey.sdkConfig.domain,
                 clientId = reachFiveKey.sdkConfig.clientId,
-                scheme = reachFiveKey.sdkConfig.scheme
+                scheme = reachFiveKey.sdkConfig.androidScheme
             ),
             providersCreators = reachFiveKey.providerCreators.map { providerCreatorInterface ->
                 Converters.providerCreatorFromInterface(providerCreatorInterface)
