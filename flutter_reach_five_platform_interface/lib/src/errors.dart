e4aa620ab56524df1b76291d3026d9971270b89e
Valentin Marquis <valentinm@bam.tech>
2022-11-14 14:04:48 +0100

    fix(platform errors): use correct docs links

--- flutter_reach_five_platform_interface/lib/src/errors.dart
+++ flutter_reach_five_platform_interface/lib/src/errors.dart
@@ -10,18 +10,18 @@ final errorCodesInterface = ErrorCodesInterface(
   userCancelledOrClosedTheWebFlow: 'user_cancelled_or_closed_the_web_flow',
 );
 
-/// {@macro flutter_reach_five.reachFiveManager.errors.email_already_in_use}
+/// {@macro flutter_reach_five.errors.email_already_in_use}
 class EmailAlreadyInUseExceptionInterface implements Exception {}
 
-/// {@macro flutter_reach_five.reachFiveManager.errors.invalid_email_or_password}
+/// {@macro flutter_reach_five.errors.invalid_email_or_password}
 class InvalidEmailOrPasswordExceptionInterface implements Exception {}
 
-/// {@macro flutter_reach_five.reachFiveManager.errors.invalid_verification_code}
+/// {@macro flutter_reach_five.errors.invalid_verification_code}
 class InvalidVerificationCodeExceptionInterface implements Exception {}
 
-/// {@macro flutter_reach_five.reachFiveManager.errors.invalid_grant_code}
+/// {@macro flutter_reach_five.errors.invalid_grant_code}
 class InvalidGrantCodeExceptionInterface implements Exception {}
 
-/// {@macro flutter_reach_five.reachFiveManager.errors.user_cancelled_or_closed_the_web_flow}
+/// {@macro flutter_reach_five.errors.user_cancelled_or_closed_the_web_flow}
 class UserCancelledOrClosedTheWebFlowCodeExceptionInterface
     implements Exception {}
